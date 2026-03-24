52782490f98f3bc3da11aec60994140256c9bcc6
yulim <eulim9926@naver.com>
2026-03-25 08:34:16 +0900

    first commit

---
+++ src/main/java/com/toy/project/studioreservationback/StudioReservationBackApplication.java
@@ -0,0 +1,13 @@
+package com.toy.project.studioreservationback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioReservationBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudioReservationBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toy/project/studioreservationback/StudioReservationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.project.studioreservationback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioReservationBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
